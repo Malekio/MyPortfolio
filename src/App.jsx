2e014b7085676e51e719ce9ec100cd9c6e26ed56
Malekio <zabdelmalek848@gmail.com>
2025-10-18 03:35:15 +0100

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <About></About>
       <Projects></Projects>
       <Contact></Contact>
+      <Footer></Footer>
     </div>
   )
 }
