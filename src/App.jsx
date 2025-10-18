fd6927e1a2cfd4a3bd2939f18c010b610d5dda06
Malekio <zabdelmalek848@gmail.com>
2025-10-18 03:17:21 +0100

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <About></About>
       <Projects></Projects>
+      <Contact></Contact>
     </div>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Contact.css"
+
+const Contact = () => {
+    return  (
+        <header className="contact">
+            <div className="contact-content">
+                <h1>LET'S CONNECT!</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Contact; 
