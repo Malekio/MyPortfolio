ddbc112d6f605aa500a91e59c6f09c799aa10d6b
Malekio <zabdelmalek848@gmail.com>
2025-10-18 02:52:52 +0100

    add About section

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <header className="about">
+            <div className="about-content">
+                <h1>ABOUT ME!</h1>
+            </div>
+        </header>
+    );
+}; 
+
+export default About;
