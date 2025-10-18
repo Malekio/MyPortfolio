bacdd7c1dd36cab9ee476ba91f91628110a58890
Malekio <zabdelmalek848@gmail.com>
2025-10-18 03:06:39 +0100

    add projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <About></About>
+      <Projects></Projects>
     </div>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Projects.css'
+
+const Projects = () => {
+    return (
+        <header className="projects">
+            <div className="projects-content">
+                <h1>MY PROJECTS</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Projects
