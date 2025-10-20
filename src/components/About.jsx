2081903664fe9c864cd2f003c8e05bad725830c4
Malekio <zabdelmalek848@gmail.com>
2025-10-20 23:46:11 +0100

    add the 'about-hero'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,14 @@ import './About.css'
 
 const About = () => {
     return (
-        <header className="about">
+        <section className="about">
             <div className="about-content">
-                <h1>ABOUT ME!</h1>
+                <h1 className="about-hero">ABOUT ME</h1>
             </div>
-        </header>
+            <div className="skills">
+                <h6 className="skills-backend">BACKEND DEVELOPER</h6>
+            </div>
+        </section>
     );
 }; 
 
